5ef09efe7d57b015893e6ad0872c5adf2f6087f7
Alain Chan <43245915+AlainDevs@users.noreply.github.com>
2024-09-20 11:30:50 +0800

    add installation shell scripts.

---
+++ requirements/lua-resty-base-encoding-master/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Update the package list
+sudo apt update
+
+# Install gcc and make
+sudo apt install -y gcc make
+
+# Run the make command
+make
+
+# Copy the librestybaseencoding.so file to the parent directory of the parent directory
+cp librestybaseencoding.so ../../
